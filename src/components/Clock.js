c2da59723032a2ce3c655b2550b340d7ebbf4730
Ishpreet <ishpreet2000@gmail.com>
2019-11-30 20:36:02 -0600

    Clock actually ticks

--- src/components/Clock.js
+++ src/components/Clock.js
@@ -5,9 +5,34 @@ import './Clock.scss';
 
 
 class Clock extends Component {
+    constructor(props) {
+        super(props);
+        this.state = {
+            date: new Date()
+        };
+    }
+    
+    componentDidMount() {
+        this.timerID = setInterval(
+            () => this.tick(),
+            1000
+        );
+    }
+
+    componentWillUnmount() {
+        clearInterval(this.timerID);
+    }
+
+    tick() {
+        this.setState({
+            date: new Date()
+        });
+    }
+    
     render() {
-        const dateString = moment().format("ddd MMM DD");
-        const timeString = moment().format("HH:mm:ss A"); 
+        const thisMoment = moment(this.state.date);
+        const dateString = thisMoment.format("ddd MMM DD");
+        const timeString = thisMoment.format("HH:mm:ss A"); 
         return (
             <div className="clock">
                 <div className="date"> { dateString } </div>
